39dd85246c9ec34ba49108858c8578df5d8009dd
Steve Nykiel <stvny@comcast.net>
2016-05-22 09:26:37 -0500

    updated main.js

--- js/main.js
+++ js/main.js
@@ -1,161 +1,46 @@
+function getIdFrom() {
+    var idFrom = "marquee-default";
+    if( $('.marquee-hide[style*="block"]').length ) {
+      idFrom = $('.marquee-hide[style*="block"]')[0].id;
+    }
+    return "#" + idFrom;
+}    
+
+function fadeFromTo(idFrom, idTo) {
+    $(idFrom).css("z-index", "99");
+    $(idTo).css("z-index", "100");
+    $(idFrom).fadeOut(1000); 
+    $(idTo).fadeIn(1000);   
+}
+
 function activateBusiness() {
-    document.getElementById("business").innerHTML = '<b>Business</b>&nbsp;<b class="sidebar-selected">-</b>';
-    document.getElementById("culture-recreation").innerHTML = 'Culture & Recreation';
-    document.getElementById("environment").innerHTML = 'Environment';
-    document.getElementById("housing-development").innerHTML = 'Housing & Development';
-    document.getElementById("social-services").innerHTML = 'Social Services';
-    document.getElementById("civic-services").innerHTML = 'Civic Services';
-    document.getElementById("education").innerHTML = 'Education';
-    document.getElementById("health").innerHTML = 'Health';
-    $("#marquee-default").fadeOut(0);
-    $("#marquee-business").fadeIn(1000);
-    $("#marquee-culture-recreation").fadeOut(0);
-    $("#marquee-environment").fadeOut(0);
-    $("#marquee-housing-development").fadeOut(0);
-    $("#marquee-social-services").fadeOut(0);
-    $("#marquee-civic-services").fadeOut(0);
-    $("#marquee-education").fadeOut(0);
-    $("#marquee-health").fadeOut(0);
+    fadeFromTo( getIdFrom(), "#marquee-business" );
 }
 
 function activateCultureRecreation() {
-    document.getElementById("business").innerHTML = 'Business';
-    document.getElementById("culture-recreation").innerHTML = '<b>Culture & Recreation</b>&nbsp;<b class="sidebar-selected">-</b>';
-    document.getElementById("environment").innerHTML = 'Environment';
-    document.getElementById("housing-development").innerHTML = 'Housing & Development';
-    document.getElementById("social-services").innerHTML = 'Social Services';
-    document.getElementById("civic-services").innerHTML = 'Civic Services';
-    document.getElementById("education").innerHTML = 'Education';
-    document.getElementById("health").innerHTML = 'Health';
-    $("#marquee-default").fadeOut(0);
-    $("#marquee-business").fadeOut(0);
-    $("#marquee-culture-recreation").fadeIn(1000);
-    $("#marquee-environment").fadeOut(0);
-    $("#marquee-housing-development").fadeOut(0);
-    $("#marquee-social-services").fadeOut(0);
-    $("#marquee-civic-services").fadeOut(0);
-    $("#marquee-education").fadeOut(0);
-    $("#marquee-health").fadeOut(0);
+    fadeFromTo( getIdFrom(), "#marquee-culture-recreation");
 }
 
 function activateEnvironment() {
-    document.getElementById("business").innerHTML = 'Business';
-    document.getElementById("culture-recreation").innerHTML = 'Culture & Recreation';
-    document.getElementById("environment").innerHTML = '<b>Environment</b>&nbsp;<b class="sidebar-selected">-</b>';
-    document.getElementById("housing-development").innerHTML = 'Housing & Development';
-    document.getElementById("social-services").innerHTML = 'Social Services';
-    document.getElementById("civic-services").innerHTML = 'Civic Services';
-    document.getElementById("education").innerHTML = 'Education';
-    document.getElementById("health").innerHTML = 'Health';
-    $("#marquee-default").fadeOut(0);
-    $("#marquee-business").fadeOut(0);
-    $("#marquee-culture-recreation").fadeOut(0);
-    $("#marquee-environment").fadeIn(1000);
-    $("#marquee-housing-development").fadeOut(0);
-    $("#marquee-social-services").fadeOut(0);
-    $("#marquee-civic-services").fadeOut(0);
-    $("#marquee-education").fadeOut(0);
-    $("#marquee-health").fadeOut(0);
+    fadeFromTo( getIdFrom(), "#marquee-environment");
 }
 
 function activateHousingDevelopment() {
-    document.getElementById("business").innerHTML = 'Business';
-    document.getElementById("culture-recreation").innerHTML = 'Culture & Recreation';
-    document.getElementById("environment").innerHTML = 'Environment';
-    document.getElementById("housing-development").innerHTML = '<b>Housing & Development</b>&nbsp;<b class="sidebar-selected">-</b>';
-    document.getElementById("social-services").innerHTML = 'Social Services';
-    document.getElementById("civic-services").innerHTML = 'Civic Services';
-    document.getElementById("education").innerHTML = 'Education';
-    document.getElementById("health").innerHTML = 'Health';
-    $("#marquee-default").fadeOut(0);
-    $("#marquee-business").fadeOut(0);
-    $("#marquee-culture-recreation").fadeOut(0);
-    $("#marquee-environment").fadeOut(0);
-    $("#marquee-housing-development").fadeIn(1000);
-    $("#marquee-social-services").fadeOut(0);
-    $("#marquee-civic-services").fadeOut(0);
-    $("#marquee-education").fadeOut(0);
-    $("#marquee-health").fadeOut(0);
+    fadeFromTo( getIdFrom(), "#marquee-housing-development");
 }
 
 function activateSocialServices() {
-    document.getElementById("business").innerHTML = 'Business';
-    document.getElementById("culture-recreation").innerHTML = 'Culture & Recreation';
-    document.getElementById("business").innerHTML = 'Business';
-    document.getElementById("culture-recreation").innerHTML = 'Culture & Recreation';
-    document.getElementById("environment").innerHTML = 'Environment';
-    document.getElementById("housing-development").innerHTML = 'Housing & Development';
-    document.getElementById("social-services").innerHTML = '<b>Social Services</b>&nbsp;<b class="sidebar-selected">-</b>';
-    document.getElementById("civic-services").innerHTML = 'Civic Services';
-    document.getElementById("education").innerHTML = 'Education';
-    document.getElementById("health").innerHTML = 'Health';
-    $("#marquee-default").fadeOut(0);
-    $("#marquee-business").fadeOut(0);
-    $("#marquee-culture-recreation").fadeOut(0);
-    $("#marquee-environment").fadeOut(0);
-    $("#marquee-housing-development").fadeOut(0);
-    $("#marquee-social-services").fadeIn(1000);
-    $("#marquee-civic-services").fadeOut(0);
-    $("#marquee-education").fadeOut(0);
-    $("#marquee-health").fadeOut(0);
+    fadeFromTo( getIdFrom(), "#marquee-social-services")
 }
 
 function activateCivicServices() {
-    document.getElementById("business").innerHTML = 'Business';
-    document.getElementById("culture-recreation").innerHTML = 'Culture & Recreation';
-    document.getElementById("environment").innerHTML = 'Environment';
-    document.getElementById("housing-development").innerHTML = 'Housing & Development';
-    document.getElementById("social-services").innerHTML = 'Social Services';
-    document.getElementById("civic-services").innerHTML = '<b>Civic Services</b>&nbsp;<b class="sidebar-selected">-</b>';
-    document.getElementById("education").innerHTML = 'Education';
-    document.getElementById("health").innerHTML = 'Health';
-    $("#marquee-default").fadeOut(0);
-    $("#marquee-business").fadeOut(0);
-    $("#marquee-culture-recreation").fadeOut(0);
-    $("#marquee-environment").fadeOut(0);
-    $("#marquee-housing-development").fadeOut(0);
-    $("#marquee-social-services").fadeOut(0);
-    $("#marquee-civic-services").fadeIn(1000);
-    $("#marquee-education").fadeOut(0);
-    $("#marquee-health").fadeOut(0);
+    fadeFromTo( getIdFrom(), "#marquee-civic-services");
 }
 
 function activateEducation() {
-    document.getElementById("business").innerHTML = 'Business';
-    document.getElementById("culture-recreation").innerHTML = 'Culture & Recreation';
-    document.getElementById("environment").innerHTML = 'Environment';
-    document.getElementById("housing-development").innerHTML = 'Housing & Development';
-    document.getElementById("social-services").innerHTML = 'Social Services';
-    document.getElementById("civic-services").innerHTML = 'Civic Services';
-    document.getElementById("education").innerHTML = '<b>Education</b>&nbsp;<b class="sidebar-selected">-</b>';
-    document.getElementById("health").innerHTML = 'Health';
-    $("#marquee-default").fadeOut(0);
-    $("#marquee-business").fadeOut(0);
-    $("#marquee-culture-recreation").fadeOut(0);
-    $("#marquee-environment").fadeOut(0);
-    $("#marquee-housing-development").fadeOut(0);
-    $("#marquee-social-services").fadeOut(0);
-    $("#marquee-civic-services").fadeOut(0);
-    $("#marquee-education").fadeIn(1000);
-    $("#marquee-health").fadeOut(0);
+    fadeFromTo( getIdFrom(), "#marquee-education");
 }
 
 function activateHealth() {
-    document.getElementById("business").innerHTML = 'Business';
-    document.getElementById("culture-recreation").innerHTML = 'Culture & Recreation';
-    document.getElementById("environment").innerHTML = 'Environment';
-    document.getElementById("housing-development").innerHTML = 'Housing & Development';
-    document.getElementById("social-services").innerHTML = 'Social Services';
-    document.getElementById("civic-services").innerHTML = 'Civic Services';
-    document.getElementById("education").innerHTML = 'Education';
-    document.getElementById("health").innerHTML = '<b>Health</b>&nbsp;<b class="sidebar-selected">-</b>';
-    $("#marquee-default").fadeOut(0);
-    $("#marquee-business").fadeOut(0);
-    $("#marquee-culture-recreation").fadeOut(0);
-    $("#marquee-environment").fadeOut(0);
-    $("#marquee-housing-development").fadeOut(0);
-    $("#marquee-social-services").fadeOut(0);
-    $("#marquee-civic-services").fadeOut(0);
-    $("#marquee-education").fadeOut(0);
-    $("#marquee-health").fadeIn(1000);
+    fadeFromTo( getIdFrom(), "#marquee-health");
 }
